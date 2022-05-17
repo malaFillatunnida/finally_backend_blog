8a951132f1ec8d3a895211df2dae69ee26ba5b48
ilham-crypto <73451629+ilham-crypto@users.noreply.github.com>
2022-05-17 12:24:32 +0700

    Update categoryController.js

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -18,13 +18,12 @@ const getAllCategories = async (req, res) => {
       {
         model: Post,
         as: "post",
-        attributes: ["id", "name", "image", "title", "description"],
+        attributes: ["id", "username", "image", "title", "description"],
       },
     ],
   });
   res.status(200).send(category);
 };
-
 
 const addPost = async (req, res) => {
   const { categoryId, postId } = req.body;
@@ -46,9 +45,6 @@ const addPost = async (req, res) => {
         }
 
         category.addPost(post);
-        console.log(
-          `>> added Post id=${post.id} to Category id=${category.id}`
-        );
         res.send(category);
       });
     })
@@ -80,7 +76,7 @@ const getOneCategory = async (req, res) => {
       {
         model: Post,
         as: "post",
-        attributes: ["id", "name", "image", "title", "description"],
+        attributes: ["id", "username", "image", "title", "description"],
       },
     ],
   });
